9b45431b10116dc2bd9ab7c738d0be252fb87804
romashka77 <romashka_77@mail.ru>
2018-08-28 14:07:09 +0300

    Объект Date. Работа с датами

--- TypeScriptTest/Scripts/js/app.js
+++ TypeScriptTest/Scripts/js/app.js
@@ -1,15 +1,39 @@
-﻿class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-    }
-    static nameToUpper(person) {
-        return person.name.toUpperCase();
-    }
-    display() {
-        console.log(this.name, this.age);
-    }
+﻿var days = ["Воскресенье", "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота"];
+var months = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь",
+    "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
+
+var myDate = new Date();
+var fullDate = "Сегодня: " + myDate.getDate() + " " + months[myDate.getMonth()] +
+    " " + myDate.getFullYear() + ", " + days[myDate.getDay()];
+document.writeln(fullDate); // Сегодня: 18 Август 2015, Вторник
+
+
+var welcome;
+var myDate = new Date();
+var hour = myDate.getHours();
+var minute = myDate.getMinutes();
+var second = myDate.getSeconds();
+if (minute < 10) {
+    minute = "0" + minute;
 }
-let tom = new Person("Tom Soyer", 34);
-let personName = Person.nameToUpper(tom);
-console.log(personName);        // TOM SOYER
+if (second < 10) {
+    second = "0" + second;
+}
+if (hour < 12) {
+    welcome = "Доброе утро";
+} else if (hour < 17) {
+    welcome = "Добрый день";
+} else {
+    welcome = "Добрый вечер";
+}
+document.writeln(welcome + ", текущее время: " + hour +
+    ":" + minute + ":" + second); // Добрый вечер, текущее время: 22:50:39
+
+var myDate = new Date();
+myDate.setDate(15);
+myDate.setMonth(6);
+myDate.setYear(2013);
+
+var fullDate2 = myDate.getDate() + " " + months[myDate.getMonth()] +
+    " " + myDate.getFullYear() + ", " + days[myDate.getDay()];
+document.writeln(fullDate2); // 15 Июль 2013, Понедельник
