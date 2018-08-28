9e72239e3db6da3ef6918b8c58249e740ef3a223
romashka_77 <romashka_77@live.ru>
2018-08-28 22:58:35 +0300

    Остановка выполнения события

--- TypeScriptTest/Scripts/js/app.js
+++ TypeScriptTest/Scripts/js/app.js
@@ -1,17 +1,15 @@
-﻿function handler(event) {
+﻿//Остановка выполнения события
+//С помощью метода preventDefault() объекта Event мы можем остановить дальнейшее выполнение события.
+function linkHandler(e) {
 
-    console.log("Тип события: " + event.type);
+    var date = new Date();
+    var hour = date.getHours();
+    console.log(hour);
+    if (hour > 12) {
 
-    console.log(event.target);
-    event.target.style.backgroundColor = "red";
+        e.preventDefault();
+        document.write("После 12 переход запрещен");
+    }
 }
-var rect = document.getElementById("rect");
-rect.addEventListener("click", handler);
-//bubbles: возвращает true, если событие является восходящим.Например, если событие возникло на вложенном элементе, то оно может быть обработано на родительском элементе.
-//cancelable: возвращает true, если можно отменить стандартную обработку события
-//currentTarget: определяет элемент, к которому прикреплен обработчик события
-//defaultPrevented: возвращает true, если был вызван у объекта Event метод preventDefault()
-//eventPhase: определяет стадию обработки события
-//target: указывает на элемент, на котором было вызвано событие
-//timeStamp: хранит время возникновения события
-//type: указывает на имя события
+var link = document.getElementById("link");
+link.addEventListener("click", linkHandler);
